946b7d535995caff0b09f6fd1daf99915c926983
LekishviliRati <lekishvili.rati@gmail.com>
2022-06-18 15:21:59 +0200

    Added Sequential Search example

---
+++ SequentialSearch.py
@@ -0,0 +1,27 @@
+"""
+Sequential Search is a simple search algorithm that checks each number in a list one by one to see if it matches
+the number we are looking for, or it is looking for.
+"""
+
+
+def sequential_search(any_list, number):
+    """
+    :param any_list:
+    :param number:
+    :return:
+    """
+    found = False
+    for i in any_list:
+        if i == number:
+            found = True
+            print("Number Found")
+            break
+    return found
+
+
+# Will print : Number found and return True
+sequential_search(range(0, 10), 7)
+
+
+
+
